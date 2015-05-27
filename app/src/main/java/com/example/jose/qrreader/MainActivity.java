cab3713c9d52bcc663c14f14978ffbe92835e98c
Jose <pepe.96.jose@gmail.com>
2015-05-27 18:49:39 -0500

    Modify layout

--- app/src/main/java/com/example/jose/qrreader/MainActivity.java
+++ app/src/main/java/com/example/jose/qrreader/MainActivity.java
@@ -1,21 +1,15 @@
 package com.example.jose.qrreader;
 
-import android.content.Context;
 import android.content.Intent;
 
 
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.app.Activity;
-import android.content.Intent;
-import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import com.google.zxing.integration.android.IntentIntegrator;
-import com.google.zxing.integration.android.IntentResult;
 
 import SampleDataBase.IndividualCreator;
 
